12c60177bf914e1593234e2851b524a4fceee36f
vasu <vasu.kokkiligadda@gmail.com>
2017-02-17 22:10:37 -0800

    testing

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+num=input("Enter number=")
+for i in range(1,num):
+    if i%3==0 and i%5==0:
+        print "FizzBuzz"
+    elif i%3==0:
+        print "Fizz"
+    elif i%5==0:
+        print "Buzz"
+    else:
+        print i
